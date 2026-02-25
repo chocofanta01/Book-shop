aa8a7e68716bb4e80143558c8ec534fda3b9599e
ChocoFanta-01 <ljh970512@naver.com>
2026-02-25 20:37:49 +0900

    깔끔하게 정리

--- controller/CartController.js
+++ controller/CartController.js
@@ -17,19 +17,20 @@ const addToCart = (req, res) => {
 }
 
 const getCartItems = (req, res) => {
-    const { user_id, selected} = req.body;
+    const { user_id, selected } = req.body;
     let sql = `SELECT cartItems.id, book_id, title, summary, quantity, price 
                 FROM cartItems LEFT JOIN books 
                 ON cartItems.book_id = books.id
                 WHERE user_id = ? AND cartItems.id IN (?)`;
-    conn.query(sql, [user_id, selected],
+    let values = [user_id, selected]
+    conn.query(sql, values,
         (err, results) => {
-        if (err) {
-            console.log(err);
-            return res.status(StatusCodes.BAD_REQUEST).end();
-        }
-        return res.status(StatusCodes.OK).json(results);
-    })
+            if (err) {
+                console.log(err);
+                return res.status(StatusCodes.BAD_REQUEST).end();
+            }
+            return res.status(StatusCodes.OK).json(results);
+        })
 }
 
 const removeCartItem = (req, res) => {
